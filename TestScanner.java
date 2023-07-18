c67d9da0876e96c0f1c03620b8b23fd16803490c
jahirsheikh <jahirsheikh14@gmail.com>
2023-07-18 13:15:30 +0600

    Add

---
+++ TestScanner.java
@@ -0,0 +1,65 @@
+
+
+package testscanner;
+
+import java.util.Scanner;
+
+
+public class TestScanner {
+
+    public static void main(String[] args) {
+     
+//        Object creation
+        Scanner sc=new Scanner(System.in);  
+        
+        
+//        System.out.println("Enter a number for radius: ");
+//            double radius=sc.nextDouble();
+//            
+//            double area = radius*radius*3.14159;
+//            
+//            System.out.println("The area for the circle of radius " + radius + " is " + area );
+        
+//        String gretting;
+//        
+//        System.out.println("pls enter gretting: ");
+//        
+//        gretting=sc.nextLine();
+//        
+//        System.out.println(gretting+ " Valo hoye jao");
+
+
+//    int n1, n2;
+//        System.out.println("Plz Enter Number 1 ");
+//        n1=sc.nextInt();
+//        
+//        System.out.println("Plz Enter Number 2");
+//            n2=sc.nextInt();
+//            
+//            int result=n1+n2;
+//            
+//            System.out.println(" The total of two number is "+result);
+//        
+//     
+//        int number, sum=0;
+//        for(int i=1; i<=10; i++){
+//            System.out.println("Plz enter number"+i);
+//            number=sc.nextInt();
+//            sum+=number;
+//        }
+//                System.out.println("Total is "+ sum);
+int n;
+int sum=0;
+
+ System.out.println("Enter a number : ");
+            n=sc.nextInt();
+        for(int i=1; i<=n; i++ ){
+        int number=sc.nextInt();
+                sum+= number;
+        }
+        System.out.println("Total " + sum);
+        
+
+    }
+    
+}
